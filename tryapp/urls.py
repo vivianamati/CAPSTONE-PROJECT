c1e9a14719ad12c443962a8a0eac40ac7ad38994
vivianamati <adeliahamati51@gmail.com>
2024-08-05 09:54:39 +0300

    made changes

--- tryapp/urls.py
+++ tryapp/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from tryapp.views import task_list, task_create, task_update, task_delete, signup, login_view, logout_view
+from tryapp.views import task_list,home, task_create, task_update, task_delete, signup, login_view, logout_view
 from tryapp import views
 from django.contrib.auth import views as auth_views
 urlpatterns = [
@@ -16,7 +16,7 @@ urlpatterns = [
     # path('accounts/', include('django.contrib.auth.urls')),  # Django's built-in auth views
     # This includes /accounts/login/, /accounts/logout/, /accounts/password_reset/, etc.
 
-    path('', views.home, name='home'),  # URL for the landing page
+    path('', home, name='home'),  # URL for the landing page
 
     path('login/', login_view, name='login'),  # Map login_view to 'login'
 
